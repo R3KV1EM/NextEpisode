6bda9dcfbf9661d491aac7e28e3d0f9894b74679
R3KV1EM <76273827+R3KV1EM@users.noreply.github.com>
2023-03-01 22:28:03 +0700

    Delete 4EpHoBuK.py

--- 4EpHoBuK.py
+++
@@ -1,9 +0,0 @@
-import re
-# Validate strong password
-password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-#re.match(password_pattern, 'secret') # Returns None
-#re.match(password_pattern, '-Secr3t.') # Returns Match object
-userpass = input("Enter your password")
-#print(re.match(password_pattern, userpass))
-if re.match(password_pattern, userpass) is None:
-    print("vayblyad")
