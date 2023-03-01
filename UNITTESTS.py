463929ee6e0e280c0c7637d6afca587e99a68554
archy <smok3rarchy@gmail.com>
2023-03-01 22:46:50 +0700

    Fresh

---
+++ UNITTESTS.py
@@ -0,0 +1,35 @@
+import unittest
+
+import UNITTESTSMAIN
+
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        testparam = 10
+        result = UNITTESTSMAIN.do_stuff(testparam)
+        self.assertEqual(result, 15)
+
+    def test_do_stuff2(self):
+        testparam = "shgas"
+        result = UNITTESTSMAIN.do_stuff(testparam)
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff3(self):
+        testparam = None
+        result = UNITTESTSMAIN.do_stuff(testparam)
+        self.assertEqual(result, 'Please enter number')
+
+    def test_do_stuff4(self):
+        testparam = ""
+        result = UNITTESTSMAIN.do_stuff(testparam)
+        self.assertEqual(result, 'Please enter number')
+
+    def test_do_stuff5(self):
+        testparam = 0
+        result = UNITTESTSMAIN.do_stuff(testparam)
+        self.assertEqual(result, 'Please enter number')
+
+    def test_do_stuff6(self):
+        testparam = (1, 2, 3, 4)
+        result = UNITTESTSMAIN.do_stuff(testparam)
+        self.assertEqual(result, 'Please enter number')
